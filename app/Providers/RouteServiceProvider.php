f79a0fda080b7870da7742094871d512970489a3
cmrxnn <cmrxn2005@gmail.com>
2022-03-11 23:59:31 +0000

    Remove unnecessary console route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                ->group(base_path('routes/app.php'));
         });
     }
 
