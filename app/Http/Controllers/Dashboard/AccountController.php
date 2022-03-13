7109f7368ff24926b756bc3ecee2994c012b817d
cmrxnn <cmrxn2005@gmail.com>
2022-03-13 15:54:53 +0000

    Add missing DB import

--- app/Http/Controllers/Dashboard/AccountController.php
+++ app/Http/Controllers/Dashboard/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Contracts\Support\Renderable;
