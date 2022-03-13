f6c1ac42728bfe12cd705e5ad510c8e4c9284fd9
Scribbl Actions <github@scribbl.ink>
2022-03-13 17:27:25 +0000

    Create release 0.1.0

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | change this value if you are not maintaining your own internal versions.
     */
 
-    'version' => 'canary',
+    'version' => '0.1.0',
 
     /*
     |--------------------------------------------------------------------------
