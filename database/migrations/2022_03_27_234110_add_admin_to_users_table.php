2a84d993043204eeb98733bfba063781d7c631a8
Cam White (Jex) <72230943+cmrxnn@users.noreply.github.com>
2022-03-29 23:29:11 +0100

    Add database table for admin permissions

---
+++ database/migrations/2022_03_27_234110_add_admin_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('admin')->default(true);
+            // CHANGE THIS BEFORE PUSHING, **PLEASE**
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIfExists('admin');
+        });
+    }
+};
